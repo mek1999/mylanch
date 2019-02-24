9efa4be1c25a78358f574037e9fa1a08daf7c19a
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-24 14:57:32 +0530

    Create arraysorting.java

---
+++ arraysorting.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n, temp;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) 
+        {
+            a[i] = s.nextInt();
+        }
+        for(int i=0;i<n;i++)  
+        {
+    	   for (int j=0;j<n;j++)  
+           {   
+             if(a[i]<a[j])  
+             {  
+                temp = a[i];  
+                a[i]=a[j];  
+                a[j] = temp;   
+             }  
+           }  
+        }  
+        for(int i=0;i<10;i++)  
+        {  
+         System.out.print(" "+a[i]);  
+        }  
+	}
+	
+}
