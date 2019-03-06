2a0111956867c5c0b1f46712e5e72578f580f33e
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-03-06 22:58:36 +0530

    Create printntimes.java

---
+++ printntimes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+    public static void main(String args[])
+    {	
+    	String str;
+    	int n;
+        Scanner s = new Scanner(System.in);
+        str=s.nextLine();
+        n=s.nextInt();
+        for(int i=0;i<n;i++)
+        {
+             System.out.println(""+str);
+        }
+    }
+}
