9624aedc2dda81307368c50f20f686ab56be6a80
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-03-08 23:34:40 +0530

    Create vala.java

---
+++ vala.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		long cn,dis,amt=1;
+		String name;
+		int sp,dp,ch;
+		Scanner s=new Scanner(System.in);
+		name=s.nextLine();
+		cn=s.nextLong();
+		sp=s.nextInt();
+		dp=s.nextInt();
+		ch=s.nextInt();
+		if(sp!=1)
+		{
+			System.out.println("Source point must be 1");
+		}
+		else if(dp>sp)
+		{	dp=dp-1;
+			dis=sp+dp;
+			
+			switch(ch)
+			{
+			case 1:amt=dis*5;
+					break;
+			case 2:amt=dis*10;
+					break;
+			case 3:amt=dis*20;
+					break;
+			default:System.out.println("No operation ,so choose choice from 1 to 2");
+					break;
+			}
+			//System.out.println(""+amt);
+			System.out.println("Name:"+name+"\nContact number:"+cn+"\nSource point:"+sp+"\nDestination point:"+dp);
+			System.out.println("Total Distance:"+dis+"\n1.Auto\n2.Two wheeler\n3.Cab");
+			System.out.println("Choice is:"+ch+"\nCharges:"+amt);
+			}
+		else
+		{
+			System.out.println("Destination point is not accepted");
+		}
+		
+	}	
+}
