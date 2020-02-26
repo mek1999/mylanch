ce160b8247f03dfd25b49b0e10e42deeaa9bc973
mek1999 <47376832+mek1999@users.noreply.github.com>
2020-02-26 19:36:33 +0530

    Create lrk value rangedivk.c

---
+++ lrk value rangedivk.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int l,r,k;
+	int count=0,i;
+	scanf("%d %d %d", &l,&r,&k);              	
+	for(i=l;i<=r;i++)
+	{
+	    if(i%k==0)
+	    count++;
+	}
+	printf("%d",count);
+}
